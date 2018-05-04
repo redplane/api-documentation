b4de5cea0ccf469d9d55ad8dc2b2c88cf49fd287
Linh Nguyen <redplane_dt@yahoo.com.vn>
2018-05-04 14:04:20 +0800

    - Added pdf export function.

--- src/app/modules/report/personal-cv/personal-cv.controller.js
+++ src/app/modules/report/personal-cv/personal-cv.controller.js
@@ -4,8 +4,13 @@ module.exports = function (ngModule) {
                   urlStateConstant,
                   candidateProfile) {
 
+            //#region Properties
+
             $scope.candidateProfile = candidateProfile;
             $scope.urlStateProject = $state.href(urlStateConstant.personalInfo.projects.name, {}, {absolute: true});
+
+            // About me information.
+            $scope.aboutMe = {};
 
             // Collection of accordion status.
             $scope.accordionStatus = {
@@ -13,10 +18,10 @@ module.exports = function (ngModule) {
                 bIsWhatIKnowOpen: true,
                 bIsTakenPartInProjectOpen: true
             };
-
             console.log(candidateProfile);
 
-            console.log($element);
+            //#endregion
+
             //#region Methods
 
             // Depend on skill point to decide class of progressbar.
@@ -36,18 +41,37 @@ module.exports = function (ngModule) {
                 return "primary";
             };
 
+            /*
+            * Print cv as an image.
+            * */
             $scope.print = function () {
                 //
                 // // Find personal profile html.
                 var personalProfileHtml = $document[0].getElementById('personalProfile');
                 html2canvas(personalProfileHtml, {
-                    onrendered: function(canvas) {
+                    onrendered: function (canvas) {
                         $scope.downloadCanvas(canvas, 'CV.png');
                     }
                 });
             };
 
-            $scope.downloadCanvas = function(canvas, fileName) {
+            /*
+            * Print cv as a pdf document.
+            * */
+            $scope.printPdf = function () {
+
+                console.log($scope.aboutMe);
+                var personalProfileHtml = $document[0].getElementById('personalProfile');
+                html2pdf(personalProfileHtml, {
+                    margin: 1,
+                    filename: $scope.aboutMe.fullName + '.pdf',
+                    image: {type: 'jpeg', quality: 1},
+                    html2canvas: {dpi: 192, letterRendering: true},
+                    jsPDF: {unit: 'in', format: 'a4', orientation: 'portrait'}
+                });
+            };
+
+            $scope.downloadCanvas = function (canvas, fileName) {
                 // get image data and transform mime type to application/octet-stream
                 var
                     canvasDataUrl = canvas.toDataURL()
@@ -67,7 +91,14 @@ module.exports = function (ngModule) {
                 } else if (anchor.click) {
                     link.click();
                 }
-            }
+            };
+
+            /*
+            * Callback which is raised when module is initialized.
+            * */
+            $scope.init = function(){
+                $scope.aboutMe = $scope.candidateProfile.aboutMe;
+            };
             //#endregion
         });
 };
