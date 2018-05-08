51e10561e9b2e506e1bf54f9bd9940b39e2cee66
Linh Nguyen <redplane_dt@yahoo.com.vn>
2018-05-09 01:18:37 +0800

    - Update bitballoon code.

--- bitballoon.js
+++ bitballoon.js
@@ -1,7 +1,13 @@
-const path = require('path');
-const dist = path.resolve('src/dist');
-const clientId = 'af09bb0f9d0abd3a341e93748caea4ae7a92a976925708708d693179b8a7a2e1';
-const clientSecret = '0625b9e63ae65dd0e1b1337fafd0ccf68679cf99134316053f6d2d306104d34e';
+// const clientId = 'af09bb0f9d0abd3a341e93748caea4ae7a92a976925708708d693179b8a7a2e1';
+// const clientSecret = '0625b9e63ae65dd0e1b1337fafd0ccf68679cf99134316053f6d2d306104d34e';
+// const accessToken = 'd99083e2e383813c4c3ffcd5b5ca8e8dc17c8c5827036a08cf8771ff96ada0c3';
+const dist = 'src/dist';
+const szCompressedDist = `${dist}\\dist.zip`;
+
+const args = require('yargs').argv;
+const siteId = args['site-id'];
+const clientId = args['client-id'];
+const clientSecret = args['client-secret'];
 
 // Import bitballoon library.
 const bitBalloon = require("bitballoon");
@@ -11,54 +17,41 @@ const zipdir = require('zip-dir');
 
 // Create bit-balloon client.
 let bitBalloonClient = bitBalloon.createClient({client_id: clientId, client_secret: clientSecret});
-
 bitBalloonClient
-    .authorizeFromCredentials((error, accessToken) => {
-        if (error)
-            return console.log(error);
+    .authorizeFromCredentials((authenticationError, accessToken) => {
+        if (authenticationError)
+            return console.log(authenticationError);
 
-        console.log(accessToken);
-        // bitBalloonClient.site('5c98ba0e-6e3a-466e-8d86-21c821ecead8', (error, site) => {
-        //     if (error)
-        //         return console.log(error);
-        //
-        //     let pathZip = `src/dist/dist.zip`;
-        //     zipdir(dist, { saveTo: '~/src/dist.zip' }, function (err, buffer) {
-        //         // `buffer` is the buffer of the zipped file
-        //         // And the buffer was saved to `~/myzip.zip`
-        //         site.createDeploy({zip: '/src/dist.zip'}, (error, deploy) => {
-        //             if (error)
-        //                 return console.log("Error updating site %o", error);
-        //
-        //             deploy.waitForReady(function (err, deploy) {
-        //                 if (err) return console.log("Error updating site %o", err);
-        //                 console.log("Site redeployed");
-        //             });
-        //         });
-        //     });
-        //
-        //
-        //
-        //     // console.log(site);
-        // });
+        // Search for bit-balloon site.
+        bitBalloonClient.site(siteId, (error, site) => {
+            if (error)
+                return console.log(error);
 
-        // // Initialize deploy options.
-        // let oDeployOptions = {
-        //     access_token: accessToken,
-        //     site_id: '5c98ba0e-6e3a-466e-8d86-21c821ecead8',
-        //     dir: 'src/dist'
-        // };
-        //
-        // console.log(accessToken);
-        //
-        // bitBalloon.deploy(oDeployOptions, (error, deploy) => {
-        //     if (error) {
-        //         console.log(deploy);
-        //         return console.log(error);
-        //     }
-        //     console.log('Deployment completed');
-        // });
+            zipdir(dist, {saveTo: szCompressedDist}, (pCompressionError, buffer) => {
+
+                if (pCompressionError)
+                    return console.log(pCompressionError);
+
+                // `buffer` is the buffer of the zipped file
+                // And the buffer was saved to `~/myzip.zip`
+                site.createDeploy({zip: szCompressedDist}, (error, deployment) => {
+                    if (error)
+                        return console.log("Error updating site %o", error);
+
+                    deployment.waitForReady(function (deploymentError, deploy) {
+                        if (deploymentError) return console.log("Error updating site %o", deploymentError);
+                        console.log('Deployment is successful.');
+                    });
+                });
+            });
+
+
+            // console.log(site);
+        });
+
 
         return accessToken;
     });
+
+
 
