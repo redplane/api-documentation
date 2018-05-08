b710f0d37190b86cdf78a550849d1da6a4a0ea2a
Linh Nguyen <redplane_dt@yahoo.com.vn>
2018-05-08 22:36:04 +0800

    - Added Bitballoon deployment to CI.

--- bitballoon.js
+++ bitballoon.js
@@ -3,7 +3,8 @@ const dist = path.resolve('src/dist');
 const clientId = 'af09bb0f9d0abd3a341e93748caea4ae7a92a976925708708d693179b8a7a2e1';
 const clientSecret = '0625b9e63ae65dd0e1b1337fafd0ccf68679cf99134316053f6d2d306104d34e';
 
-let bitBalloon = require("bitballoon");
+// Import bitballoon library.
+const bitBalloon = require("bitballoon");
 let bitBalloonClient = bitBalloon.createClient({client_id: clientId, client_secret: clientSecret});
 
 bitBalloonClient
@@ -18,21 +19,16 @@ bitBalloonClient
             dir: dist
         };
 
-        bitBalloon.deploy(oDeployOptions,  (error, deploy) => {
-            if (error)
+        console.log(accessToken);
+
+        bitBalloon.deploy(oDeployOptions, (error, deploy) => {
+            if (error) {
+                console.log(deploy);
                 return console.log(error);
+            }
             console.log('Deployment completed');
         });
 
         return accessToken;
-        // Client is now ready to do requests
-        // You can store the access_token to avoid authorizing in the future
     });
 
-//
-// bitBalloonClient.sites(function (err, sites) {
-//     for (let site of sites) {
-//         // console.log(site.id);
-//         console.log(`${site.id} = ${site.name}`);
-//     }
-// });
